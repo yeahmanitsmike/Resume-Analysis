a6b724df3103192018c1350e3c885909d76de3fa
yeahmanitsmike <yeahmanitsmike@gmail.com>
2025-02-09 14:56:40 -0600

    init commit

---
+++ output_relevant_bullets.py
@@ -0,0 +1,2 @@
+# script to input the dictionary of categorized bullets from job description and output a dictionary 
+# of relevant bullets from my resume bullet table 
---
+++ read_resume_files.py
@@ -0,0 +1,11 @@
+#Script to read resume files and extract keywords from resume
+
+#pull file from drive location
+
+#read as a docx file
+
+#input a list of keywords to search for 
+
+#separate and categorize bullet points
+
+#search bullet points and match list of keywords on the bullet points 
